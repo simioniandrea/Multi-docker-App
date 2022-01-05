a72182fcfe94bce1f932086a69b6ddc563b29e93
simioniandrea <andreasimioni5@gmail.com>
2022-01-05 16:27:44 +0100

    add files

---
+++ server/keys.js
@@ -0,0 +1,11 @@
+// ENVIRONMENT VARIABLE  
+module.exports = {
+  // REDIS_HOST = THE NAME OF VAR THAT THERE IS IN ENVIRONMENT SECTION OF DOCKER COMPOSE
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+};
